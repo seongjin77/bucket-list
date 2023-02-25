904bcef8253c965e365f2dbe8bd369511ae3f8b0
seongjin <kssj9056@gmail.com>
2023-02-25 11:01:13 +0900

    💄 Design: 글꼴 변경

--- src/App.js
+++ src/App.js
@@ -6,40 +6,49 @@ import Login from "./pages/login/Login";
 import Sign from "./pages/sign/Sign";
 import Todo from "./pages/todo/Todo";
 import { PrivateRoute, PublicRoute } from "./router/Router";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 function App() {
+    const theme = createTheme({
+        typography: {
+            fontFamily: ['Jua', 'sans-serif'].join(',') 
+        }
+    });
+
     return (
-        <Container maxWidth="md">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Navigate to="/todo" />} />
-                <Route
-                    path="/todo"
-                    element={
-                        <PrivateRoute>
-                            <Todo />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/signin"
-                    element={
-                        <PublicRoute>
-                            <Login />
-                        </PublicRoute>
-                    }
-                />
-                <Route
-                    path="/signup"
-                    element={
-                        <PublicRoute>
-                            <Sign />
-                        </PublicRoute>
-                    }
-                />
-                <Route path="/*" element={<Error />} />
-            </Routes>
-        </Container>
+        <ThemeProvider theme={theme}>
+            <Container maxWidth="md">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Navigate to="/todo" />} />
+                    <Route
+                        path="/todo"
+                        element={
+                            <PrivateRoute>
+                                <Todo />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/signin"
+                        element={
+                            <PublicRoute>
+                                <Login />
+                            </PublicRoute>
+                        }
+                    />
+                    <Route
+                        path="/signup"
+                        element={
+                            <PublicRoute>
+                                <Sign />
+                            </PublicRoute>
+                        }
+                    />
+                    <Route path="/*" element={<Error />} />
+                </Routes>
+            </Container>
+        </ThemeProvider>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
     <BrowserRouter>
-        <GlobalStyle/>
         <App />
     </BrowserRouter>
 );
